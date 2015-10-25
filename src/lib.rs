c96c4cae8e70efc323971c2f85c243666167a140
jmagnuson <magnu225@umn.edu>
2015-10-25 12:46:57 -0500

    Fix compiler errors for unknown and experimental attributes

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,15 @@
 #![allow(unused_attributes)]
-#![feature(custom_attribute)]
-#![cfg_attr(test, feature(test))]
+//#![feature(custom_attribute)]
+//#![cfg_attr(test, feature(test))]
 
 #![crate_name = "ahrs"]
-#![comment = "Madgwick AHRS algorithm"]
-#![license = "GPLv2"]
+//#![comment = "Madgwick AHRS algorithm"]
+//#![license = "GPLv2"]
 
 //#![warn(missing_doc)]
 
-#[cfg(test)]
-extern crate test;
+//#[cfg(test)]
+//extern crate test;
 
 extern crate nalgebra as na;
 
