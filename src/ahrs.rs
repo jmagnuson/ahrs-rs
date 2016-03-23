b063617d8eb68fe467c0240e5a6994bbc719772c
jmagnuson <magnu225@umn.edu>
2016-03-23 00:37:53 -0500

    Refactor to rust naming conventions

--- src/ahrs.rs
+++ src/ahrs.rs
@@ -5,61 +5,61 @@
 use na::{Vec2, Vec3, Vec6, Mat6, Norm, Cross, Quat};
 use na;
 
-//#[derive(Copy)]
+#[derive(Debug, Clone, Copy)]
 pub struct AHRS {
 
-  SamplePeriod: f64,
-  pub Quaternion:   Quat<f64>,
-  Kp:           f64,
-  Ki:           f64,
-  KpInit:       f64,
-  InitPeriod:   f64,
+  sample_period: f64,
+  pub quat: Quat<f64>,
+  kp: f64,
+  ki: f64,
+  kp_init: f64,
+  init_period: f64,
 
-  q:            Quat<f64>,
-  IntError:     Vec3<f64>,
-  KpRamped:     f64,
+  q: Quat<f64>,
+  int_error: Vec3<f64>,
+  kp_ramped: f64,
 
-  // temporary until Update() is written properly
-  Beta:         f64
+  // temporary until update() is written properly
+  beta: f64
 }
 
 impl AHRS {
 
   pub fn default() -> AHRS {
-    AHRS { SamplePeriod: (1.0f64)/(256.0f64), Quaternion: Quat::new(1.0f64, 0.0, 0.0, 0.0),
-           Kp: 1.0f64, Ki: 0.0f64, KpInit: 1.0, InitPeriod: 5.0f64, q: Quat::new(1.0f64, 0.0, 0.0, 0.0),
-           IntError: Vec3::new(0.0f64, 0.0, 0.0), KpRamped: 0.0f64, Beta: 1.0f64
+    AHRS { sample_period: (1.0f64)/(256.0f64), quat: Quat::new(1.0f64, 0.0, 0.0, 0.0),
+           kp: 1.0f64, ki: 0.0f64, kp_init: 1.0, init_period: 5.0f64, q: Quat::new(1.0f64, 0.0, 0.0, 0.0),
+           int_error: Vec3::new(0.0f64, 0.0, 0.0), kp_ramped: 0.0f64, beta: 1.0f64
          }
   }
 
-  pub fn Update( &mut self, Gyroscope: Vec3<f64>, Accelerometer: Vec3<f64>, Magnetometer: Vec3<f64> ) -> bool {
+  pub fn update( &mut self, gyroscope: Vec3<f64>, accelerometer: Vec3<f64>, magnetometer: Vec3<f64> ) -> bool {
     
     // Nomralize accelerometer measurement
-    if Norm::norm(&Accelerometer) > 0.0f64 {
-      na::normalize( &Accelerometer );
+    if Norm::norm(&accelerometer) > 0.0f64 {
+      na::normalize( &accelerometer );
     } else {
       return false;
     }
     
     // Nomralize magnetometer measurement
-    if Norm::norm(&Magnetometer) > 0.0f64 {
-      na::normalize( &Magnetometer );
+    if Norm::norm(&magnetometer) > 0.0f64 {
+      na::normalize( &magnetometer );
     } else {
       return false;
     }
 
     // Reference direction of Earth's magnetic field (Quaternion should still be conj of q)
-    let h = self.q * ( Quat::new(0.0f64, Magnetometer[0], Magnetometer[1], Magnetometer[2]) * self.Quaternion );
+    let h = self.q * ( Quat::new(0.0f64, magnetometer[0], magnetometer[1], magnetometer[2]) * self.quat );
     let b = Quat::new( 0.0f64, Norm::norm(&Vec2::new(h[2], h[3])), 0.0, h[4] );
 
     // Gradient descent algorithm corrective step
     let F = Vec6::new(
-      2.0*(self.q[1]*self.q[3] - self.q[0]*self.q[2]) - Accelerometer[0],
-      2.0*(self.q[0]*self.q[1] + self.q[2]*self.q[3]) - Accelerometer[1],
-      2.0*(0.5 - self.q[1]*self.q[1] - self.q[2]*self.q[2]) - Accelerometer[2],
-      2.0*b[1]*(0.5 - self.q[2]*self.q[2] - self.q[3]*self.q[3]) + 2.0*b[3]*(self.q[1]*self.q[3] - self.q[0]*self.q[2]) - Magnetometer[0],
-      2.0*b[1]*(self.q[1]*self.q[2] - self.q[0]*self.q[3]) + 2.0*b[3]*(self.q[0]*self.q[1] + self.q[2]*self.q[3]) - Magnetometer[1],
-      2.0*b[1]*(self.q[0]*self.q[2] + self.q[1]*self.q[3]) + 2.0*b[3]*(0.5 - self.q[1]*self.q[1] - self.q[2]*self.q[2]) - Magnetometer[2] );
+      2.0*(self.q[1]*self.q[3] - self.q[0]*self.q[2]) - accelerometer[0],
+      2.0*(self.q[0]*self.q[1] + self.q[2]*self.q[3]) - accelerometer[1],
+      2.0*(0.5 - self.q[1]*self.q[1] - self.q[2]*self.q[2]) - accelerometer[2],
+      2.0*b[1]*(0.5 - self.q[2]*self.q[2] - self.q[3]*self.q[3]) + 2.0*b[3]*(self.q[1]*self.q[3] - self.q[0]*self.q[2]) - magnetometer[0],
+      2.0*b[1]*(self.q[1]*self.q[2] - self.q[0]*self.q[3]) + 2.0*b[3]*(self.q[0]*self.q[1] + self.q[2]*self.q[3]) - magnetometer[1],
+      2.0*b[1]*(self.q[0]*self.q[2] + self.q[1]*self.q[3]) + 2.0*b[3]*(0.5 - self.q[1]*self.q[1] - self.q[2]*self.q[2]) - magnetometer[2] );
    
     let J_t = Mat6::new(
       -2.0*self.q[2], 2.0*self.q[1], 0.0, -2.0*b[3]*self.q[2], -2.0*(b[1]*self.q[3]+b[3]*self.q[1]), 2.0*b[1]*self.q[2],
@@ -73,27 +73,25 @@ impl AHRS {
     na::normalize( &step ); // normalize step magnitude
 
     // Compute rate of change for quaternion
-    let qDot:Quat<f64> = self.q * Quat::new(0.0f64, Gyroscope[0], Gyroscope[1], Gyroscope[2])
-        * 0.5 - Quat::new(step[0], step[1], step[2], step[3]) * self.Beta;
+    let qDot:Quat<f64> = self.q * Quat::new(0.0f64, gyroscope[0], gyroscope[1], gyroscope[2])
+        * 0.5 - Quat::new(step[0], step[1], step[2], step[3]) * self.beta;
 
     // Integrate to yield quaternion
-    self.q = self.q + qDot * self.SamplePeriod;
-    self.Quaternion = self.q;
-    na::normalize( &self.Quaternion );
+    self.q = self.q + qDot * self.sample_period;
+    self.quat = self.q;
+    na::normalize( &self.quat );
 
-    return true;
+    true
   }
 
-  pub fn UpdateIMU( &mut self, Gyroscope: Vec3<f64>, Accelerometer: Vec3<f64> ) -> bool {
+  pub fn update_imu( &mut self, gyroscope: Vec3<f64>, accelerometer: Vec3<f64> ) -> bool {
     
-    let mut accel:Vec3<f64>;
-
     // Normalize accelerometer measurement
-    if Norm::norm(&Accelerometer) > 0.0f64 {
-      accel = Accelerometer.normalize();
-    } else {
+    if Norm::norm(&accelerometer) <= 0.0f64 {
       return false;
     }
+
+    let accel = accelerometer.normalize();
 
     // Compute error between estimated and measured directly of gravity
     let v = Vec3::new(
@@ -105,38 +103,38 @@ impl AHRS {
     let error:Vec3<f64> = Cross::cross( &v, &accel );
 
     // Compute ramped Kp value used during init period
-    //if self.KpRamped > self.Kp {
-    //  self.IntError = Vec3::new(0.0f64, 0.0, 0.0);
-    //  self.KpRamped = self.KpRamped - ( self.KpInit - self.Kp ) / ( self.InitPeriod / self.SamplePeriod );
+    //if self.kp_ramped > self.kp {
+    //  self.int_error = Vec3::new(0.0f64, 0.0, 0.0);
+    //  self.kp_ramped = self.kp_ramped - ( self.kp_init - self.kp ) / ( self.init_period / self.sample_period );
     //} else {
-    //  self.KpRamped = self.Kp;
-      self.IntError = self.IntError + error;
+    //  self.kp_ramped = self.kp;
+      self.int_error = self.int_error + error;
     //}
 
     // Apply feedback terms
-    let Ref:Vec3<f64> = Gyroscope - error*self.Kp - self.IntError*self.Ki;
+    let Ref:Vec3<f64> = gyroscope - error*self.kp - self.int_error*self.ki;
 
     // Compute rate of change of quaternion
     let pDot:Quat<f64> = ( self.q * Quat::new( 0.0f64, Ref.x, Ref.y, Ref.z ) ) * 0.5f64;
-    self.q = self.q + pDot * self.SamplePeriod;
+    self.q = self.q + pDot * self.sample_period;
     self.q.normalize_mut();
 
     // Store conjugate
-    self.Quaternion = self.q.clone();
-    self.Quaternion.conjugate_mut();
+    self.quat = self.q;
+    self.quat.conjugate_mut();
 
-    return true;
+    true
   }
 
   pub fn Reset( &mut self ) {
-    self.KpRamped = self.KpInit;
-    self.IntError = Vec3::new(0.0f64, 0.0, 0.0);
+    self.kp_ramped = self.kp_init;
+    self.int_error = Vec3::new(0.0f64, 0.0, 0.0);
     self.q = Quat::new(1.0f64, 0.0, 0.0, 0.0);
   }
 
-  pub fn StepDown( &mut self, Kp_in: f64 ) {
-    self.KpRamped = self.Kp;
-    self.Kp = Kp_in;
+  pub fn StepDown( &mut self, kp_in: f64 ) {
+    self.kp_ramped = self.kp;
+    self.kp = kp_in;
   }
 
 }
--- tests/unit.rs
+++ tests/unit.rs
@@ -9,7 +9,6 @@ mod test{
 
   use ahrs::AHRS;
   use na::{Vec3,Quat,ApproxEq};
-  //use na;
 
   #[test]
   fn test_equal_quats() {
@@ -21,17 +20,14 @@ mod test{
 
     let mut ahrs = AHRS::default();
     let accel = Vec3::new(-3.102632460623745e-02f64, 3.049616351072091e-02, 9.721632321637426e-01);
-    let gyro = Vec3::new(0.0f64, 0.0, 0.0); //Vec3::new(0.056f64, 0.8740, -0.4510);
+    let gyro = Vec3::new(0.0f64, 0.0, 0.0);
     let mag = Vec3::new(1.4f64, 1.4, 1.2);
 
     for i in 1i64..2000 {
-        ahrs.UpdateIMU(gyro, accel);
+        ahrs.update_imu(gyro, accel);
     }
 
-    //println!("computed : {}\t{}\t{}\t{}", ahrs.Quaternion[0], ahrs.Quaternion[1], ahrs.Quaternion[2], ahrs.Quaternion[3]);
-    //println!("expected : 0.999750\t-0.015666\t-0.015939\t-2.661844e-18");
-    
-    assert_approx_eq!( &ahrs.Quaternion, &Quat::new( 0.999750219795400f64,
+    assert_approx_eq!( &ahrs.quat, &Quat::new( 0.999750219795400f64,
                                                         -0.015666683681890,
                                                         -0.015939041422266,
                                                         -2.661844518290388e-18
