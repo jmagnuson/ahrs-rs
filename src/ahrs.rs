c3d25ba0548933d3ad9ba9bc50a695bb74308ef2
jmagnuson <magnu225@umn.edu>
2016-08-18 17:30:47 -0500

    Update to latest nalgebra

--- src/ahrs.rs
+++ src/ahrs.rs
@@ -2,21 +2,21 @@
 #![allow(non_snake_case)]
 #![allow(dead_code)]       // unused placeholder variables
 
-use na::{Vec2, Vec3, Vec6, Mat6, Norm, Cross, Quat};
+use na::{Vector2, Vector3, Vector6, Matrix6, Norm, Cross, Quaternion};
 use na;
 
 #[derive(Debug, Clone, Copy)]
 pub struct AHRS {
 
   sample_period: f64,
-  pub quat: Quat<f64>,
+  pub quat: Quaternion<f64>,
   kp: f64,
   ki: f64,
   kp_init: f64,
   init_period: f64,
 
-  q: Quat<f64>,
-  int_error: Vec3<f64>,
+  q: Quaternion<f64>,
+  int_error: Vector3<f64>,
   kp_ramped: f64,
 
   // temporary until update() is written properly
@@ -26,13 +26,13 @@ pub struct AHRS {
 impl AHRS {
 
   pub fn default() -> AHRS {
-    AHRS { sample_period: (1.0f64)/(256.0f64), quat: Quat::new(1.0f64, 0.0, 0.0, 0.0),
-           kp: 1.0f64, ki: 0.0f64, kp_init: 1.0, init_period: 5.0f64, q: Quat::new(1.0f64, 0.0, 0.0, 0.0),
-           int_error: Vec3::new(0.0f64, 0.0, 0.0), kp_ramped: 0.0f64, beta: 1.0f64
+    AHRS { sample_period: (1.0f64)/(256.0f64), quat: Quaternion::new(1.0f64, 0.0, 0.0, 0.0),
+           kp: 1.0f64, ki: 0.0f64, kp_init: 1.0, init_period: 5.0f64, q: Quaternion::new(1.0f64, 0.0, 0.0, 0.0),
+           int_error: Vector3::new(0.0f64, 0.0, 0.0), kp_ramped: 0.0f64, beta: 1.0f64
          }
   }
 
-  pub fn update( &mut self, gyroscope: Vec3<f64>, accelerometer: Vec3<f64>, magnetometer: Vec3<f64> ) -> bool {
+  pub fn update( &mut self, gyroscope: Vector3<f64>, accelerometer: Vector3<f64>, magnetometer: Vector3<f64> ) -> bool {
     
     // Nomralize accelerometer measurement
     if Norm::norm(&accelerometer) > 0.0f64 {
@@ -49,11 +49,11 @@ impl AHRS {
     }
 
     // Reference direction of Earth's magnetic field (Quaternion should still be conj of q)
-    let h = self.q * ( Quat::new(0.0f64, magnetometer[0], magnetometer[1], magnetometer[2]) * self.quat );
-    let b = Quat::new( 0.0f64, Norm::norm(&Vec2::new(h[2], h[3])), 0.0, h[4] );
+    let h = self.q * ( Quaternion::new(0.0f64, magnetometer[0], magnetometer[1], magnetometer[2]) * self.quat );
+    let b = Quaternion::new( 0.0f64, Norm::norm(&Vector2::new(h[2], h[3])), 0.0, h[4] );
 
     // Gradient descent algorithm corrective step
-    let F = Vec6::new(
+    let F = Vector6::new(
       2.0*(self.q[1]*self.q[3] - self.q[0]*self.q[2]) - accelerometer[0],
       2.0*(self.q[0]*self.q[1] + self.q[2]*self.q[3]) - accelerometer[1],
       2.0*(0.5 - self.q[1]*self.q[1] - self.q[2]*self.q[2]) - accelerometer[2],
@@ -61,7 +61,7 @@ impl AHRS {
       2.0*b[1]*(self.q[1]*self.q[2] - self.q[0]*self.q[3]) + 2.0*b[3]*(self.q[0]*self.q[1] + self.q[2]*self.q[3]) - magnetometer[1],
       2.0*b[1]*(self.q[0]*self.q[2] + self.q[1]*self.q[3]) + 2.0*b[3]*(0.5 - self.q[1]*self.q[1] - self.q[2]*self.q[2]) - magnetometer[2] );
    
-    let J_t = Mat6::new(
+    let J_t = Matrix6::new(
       -2.0*self.q[2], 2.0*self.q[1], 0.0, -2.0*b[3]*self.q[2], -2.0*(b[1]*self.q[3]+b[3]*self.q[1]), 2.0*b[1]*self.q[2],
       2.0*self.q[3], 2.0*self.q[0], -4.0*self.q[1], 2.0*b[3]*self.q[3], 2.0*(b[1]*self.q[2]+b[3]*self.q[0]), 2.0*(b[1]*self.q[3]-2.0*b[3]*self.q[1]),
       -2.0*self.q[0], 2.0*self.q[3], -4.0*self.q[2], -2.0*(2.0*b[1]*self.q[2]-b[3]*self.q[0]), 2.0*(b[1]*self.q[1]+b[3]*self.q[3]), 2.0*(b[1]*self.q[0]-2.0*b[3]*self.q[2]),
@@ -69,12 +69,12 @@ impl AHRS {
       0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
       0.0, 0.0, 0.0, 0.0, 0.0, 0.0 );
    
-    let step:Vec6<f64> = J_t * F;
+    let step:Vector6<f64> = J_t * F;
     na::normalize( &step ); // normalize step magnitude
 
     // Compute rate of change for quaternion
-    let qDot:Quat<f64> = self.q * Quat::new(0.0f64, gyroscope[0], gyroscope[1], gyroscope[2])
-        * 0.5 - Quat::new(step[0], step[1], step[2], step[3]) * self.beta;
+    let qDot:Quaternion<f64> = self.q * Quaternion::new(0.0f64, gyroscope[0], gyroscope[1], gyroscope[2])
+        * 0.5 - Quaternion::new(step[0], step[1], step[2], step[3]) * self.beta;
 
     // Integrate to yield quaternion
     self.q = self.q + qDot * self.sample_period;
@@ -84,7 +84,7 @@ impl AHRS {
     true
   }
 
-  pub fn update_imu( &mut self, gyroscope: Vec3<f64>, accelerometer: Vec3<f64> ) -> bool {
+  pub fn update_imu( &mut self, gyroscope: Vector3<f64>, accelerometer: Vector3<f64> ) -> bool {
     
     // Normalize accelerometer measurement
     if Norm::norm(&accelerometer) <= 0.0f64 {
@@ -94,17 +94,17 @@ impl AHRS {
     let accel = accelerometer.normalize();
 
     // Compute error between estimated and measured directly of gravity
-    let v = Vec3::new(
+    let v = Vector3::new(
               2.0f64*(self.q.i*self.q.k - self.q.w*self.q.j),
               2.0f64*(self.q.w*self.q.i + self.q.j*self.q.k),
                     self.q.w*self.q.w - self.q.i*self.q.i
                     - self.q.j*self.q.j + self.q.k*self.q.k
     );
-    let error:Vec3<f64> = Cross::cross( &v, &accel );
+    let error:Vector3<f64> = Cross::cross( &v, &accel );
 
     // Compute ramped Kp value used during init period
     //if self.kp_ramped > self.kp {
-    //  self.int_error = Vec3::new(0.0f64, 0.0, 0.0);
+    //  self.int_error = Vector3::new(0.0f64, 0.0, 0.0);
     //  self.kp_ramped = self.kp_ramped - ( self.kp_init - self.kp ) / ( self.init_period / self.sample_period );
     //} else {
     //  self.kp_ramped = self.kp;
@@ -112,10 +112,10 @@ impl AHRS {
     //}
 
     // Apply feedback terms
-    let Ref:Vec3<f64> = gyroscope - error*self.kp - self.int_error*self.ki;
+    let Ref:Vector3<f64> = gyroscope - error*self.kp - self.int_error*self.ki;
 
     // Compute rate of change of quaternion
-    let pDot:Quat<f64> = ( self.q * Quat::new( 0.0f64, Ref.x, Ref.y, Ref.z ) ) * 0.5f64;
+    let pDot:Quaternion<f64> = ( self.q * Quaternion::new( 0.0f64, Ref.x, Ref.y, Ref.z ) ) * 0.5f64;
     self.q = self.q + pDot * self.sample_period;
     self.q.normalize_mut();
 
@@ -128,8 +128,8 @@ impl AHRS {
 
   pub fn Reset( &mut self ) {
     self.kp_ramped = self.kp_init;
-    self.int_error = Vec3::new(0.0f64, 0.0, 0.0);
-    self.q = Quat::new(1.0f64, 0.0, 0.0, 0.0);
+    self.int_error = Vector3::new(0.0f64, 0.0, 0.0);
+    self.q = Quaternion::new(1.0f64, 0.0, 0.0, 0.0);
   }
 
   pub fn StepDown( &mut self, kp_in: f64 ) {
--- tests/unit.rs
+++ tests/unit.rs
@@ -8,26 +8,26 @@ extern crate ahrs;
 mod test{
 
   use ahrs::AHRS;
-  use na::{Vec3,Quat,ApproxEq};
+  use na::{Vector3,Quaternion,ApproxEq};
 
   #[test]
   fn test_equal_quats() {
-      assert_approx_eq!( &Quat::new(1.0f64, 2.0, 3.0, 4.0), &Quat::new(1.0f64, 2.0, 3.0, 4.0) );
+      assert_approx_eq!( &Quaternion::new(1.0f64, 2.0, 3.0, 4.0), &Quaternion::new(1.0f64, 2.0, 3.0, 4.0) );
   }
 
   #[test]
   fn test_update_known_vals() {
 
     let mut ahrs = AHRS::default();
-    let accel = Vec3::new(-3.102632460623745e-02f64, 3.049616351072091e-02, 9.721632321637426e-01);
-    let gyro = Vec3::new(0.0f64, 0.0, 0.0);
-    let mag = Vec3::new(1.4f64, 1.4, 1.2);
+    let accel = Vector3::new(-3.102632460623745e-02f64, 3.049616351072091e-02, 9.721632321637426e-01);
+    let gyro = Vector3::new(0.0f64, 0.0, 0.0);
+    let mag = Vector3::new(1.4f64, 1.4, 1.2);
 
     for i in 1i64..2000 {
         ahrs.update_imu(gyro, accel);
     }
 
-    assert_approx_eq!( &ahrs.quat, &Quat::new( 0.999750219795400f64,
+    assert_approx_eq!( &ahrs.quat, &Quaternion::new( 0.999750219795400f64,
                                                         -0.015666683681890,
                                                         -0.015939041422266,
                                                         -2.661844518290388e-18
