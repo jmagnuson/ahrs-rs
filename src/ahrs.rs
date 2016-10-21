0b1b62461119fbbbe0b93499ba695767e92f1ca8
jmagnuson <magnu225@umn.edu>
2016-10-20 21:28:18 -0500

    Accept generic `N: BaseFloat` parameters
    Ahrs was previously limited to <f64>.

--- src/ahrs.rs
+++ src/ahrs.rs
@@ -2,75 +2,79 @@
 #![allow(non_snake_case)]
 #![allow(dead_code)]
 
-use na::{Vector2, Vector3, Vector4, Vector6, Matrix6, Matrix4, Norm, Quaternion, try_normalize};
+use na::{Vector2, Vector3, Vector4, Vector6, Matrix6, Matrix4, Norm, Quaternion, try_normalize, BaseFloat};
 use na;
 
-#[derive(Debug, Clone, Copy)]
-pub struct Ahrs {
+#[derive(Eq, PartialEq, Clone, Debug, Hash, Copy)]
+pub struct Ahrs<N: BaseFloat> {
 
-  sample_period: f64,
-  beta: f64,
-  pub quat: Quaternion<f64>
+  sample_period: N,
+  beta: N,
+  pub quat: Quaternion<N>
 
 }
 
-impl Default for Ahrs {
+impl Default for Ahrs<f64> {
 
   /// Creates a new Ahrs instance with default filter parameters.
-  fn default() -> Ahrs {
-    Ahrs { sample_period: (1.0f64)/(256.0f64),
+  fn default() -> Ahrs<f64> {
+    Ahrs { sample_period: (1.0f64)/(256.0),
            beta: 0.1f64,
            quat: Quaternion::new(1.0f64, 0.0, 0.0, 0.0)
          }
   }
 
-
 }
 
-impl Ahrs {
+impl<N: BaseFloat> Ahrs<N> {
 
   /// Updates the current state quaternion using 9dof IMU values.
-  pub fn update( &mut self, gyroscope: Vector3<f64>, accelerometer: Vector3<f64>, magnetometer: Vector3<f64> ) -> bool {
+  pub fn update( &mut self, gyroscope: Vector3<N>, accelerometer: Vector3<N>, magnetometer: Vector3<N> ) -> bool {
     let q = self.quat;
     
+    let zero: N = na::zero();
+    let two: N = na::cast(2.0);
+    let four: N = na::cast(4.0);
+    let half: N = na::cast(0.5);
+
     // Nomralize accelerometer measurement
-    let accel = match try_normalize(&accelerometer, 0.0f64) {
+    let accel = match try_normalize(&accelerometer, zero) {
         Some(n) => n,
         None => { return false; }
     };
     
     // Nomralize magnetometer measurement
-    let mag = match try_normalize(&magnetometer, 0.0f64) {
+    let mag = match try_normalize(&magnetometer, zero) {
         Some(n) => n,
         None => { return false; }
     };
 
     // Reference direction of Earth's magnetic field (Quaternion should still be conj of q)
-    let h = q * ( Quaternion::new(0.0f64, mag[0], mag[1], mag[2]) * self.quat );
-    let b = Quaternion::new( 0.0f64, Norm::norm(&Vector2::new(h[1], h[2])), 0.0, h[3] );
+    let h = q * ( Quaternion::from_parts(zero, mag) );
+    let b = Quaternion::new( zero, Norm::norm(&Vector2::new(h[1], h[2])), zero, h[3] );
 
     // Gradient descent algorithm corrective step
     let F = Vector6::new(
-      2.0*(       q[1]*q[3] - q[0]*q[2]) - accel[0],
-      2.0*(       q[0]*q[1] + q[2]*q[3]) - accel[1],
-      2.0*( 0.5 - q[1]*q[1] - q[2]*q[2]) - accel[2],
-      2.0*b[1]*(0.5 - q[2]*q[2] - q[3]*q[3]) + 2.0*b[3]*(q[1]*q[3] - q[0]*q[2]) - mag[0],
-      2.0*b[1]*(q[1]*q[2] - q[0]*q[3]) + 2.0*b[3]*(      q[0]*q[1] + q[2]*q[3]) - mag[1],
-      2.0*b[1]*(q[0]*q[2] + q[1]*q[3]) + 2.0*b[3]*(0.5 - q[1]*q[1] - q[2]*q[2]) - mag[2] );
+      two*(       q[1]*q[3] - q[0]*q[2]) - accel[0],
+      two*(       q[0]*q[1] + q[2]*q[3]) - accel[1],
+      two*(half - q[1]*q[1] - q[2]*q[2]) - accel[2],
+      two*b[1]*(half - q[2]*q[2] - q[3]*q[3]) + two*b[3]*(q[1]*q[3] - q[0]*q[2]) - mag[0],
+      two*b[1]*(q[1]*q[2] - q[0]*q[3]) + two*b[3]*(       q[0]*q[1] + q[2]*q[3]) - mag[1],
+      two*b[1]*(q[0]*q[2] + q[1]*q[3]) + two*b[3]*(half - q[1]*q[1] - q[2]*q[2]) - mag[2] );
    
     let J_t = Matrix6::new(
-      -2.0*q[2], 2.0*q[1],       0.0, -2.0*b[3]*q[2],                 -2.0*(b[1]*q[3]+b[3]*q[1]), 2.0*b[1]*q[2],
-       2.0*q[3], 2.0*q[0], -4.0*q[1],  2.0*b[3]*q[3],                  2.0*(b[1]*q[2]+b[3]*q[0]), 2.0*(b[1]*q[3]-2.0*b[3]*q[1]),
-      -2.0*q[0], 2.0*q[3], -4.0*q[2], -2.0*(2.0*b[1]*q[2]-b[3]*q[0]),  2.0*(b[1]*q[1]+b[3]*q[3]), 2.0*(b[1]*q[0]-2.0*b[3]*q[2]),
-       2.0*q[1], 2.0*q[2],       0.0, -2.0*(2.0*b[1]*q[3]+b[3]*q[1]), -2.0*(b[1]*q[0]+b[3]*q[2]), 2.0*b[1]*q[1],
-      0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
-      0.0, 0.0, 0.0, 0.0, 0.0, 0.0 );
+      -two*q[2], two*q[1],       zero, -two*b[3]*q[2],                 -two*(b[1]*q[3]+b[3]*q[1]), two*b[1]*q[2],
+       two*q[3], two*q[0], -four*q[1],  two*b[3]*q[3],                  two*(b[1]*q[2]+b[3]*q[0]), two*(b[1]*q[3]-two*b[3]*q[1]),
+      -two*q[0], two*q[3], -four*q[2], -two*(two*b[1]*q[2]-b[3]*q[0]),  two*(b[1]*q[1]+b[3]*q[3]), two*(b[1]*q[0]-two*b[3]*q[2]),
+       two*q[1], two*q[2],       zero, -two*(two*b[1]*q[3]+b[3]*q[1]), -two*(b[1]*q[0]+b[3]*q[2]), two*b[1]*q[1],
+      zero, zero, zero, zero, zero, zero,
+      zero, zero, zero, zero, zero, zero );
    
-    let step:Vector6<f64> = na::normalize(&(J_t * F));
+    let step = na::normalize(&(J_t * F));
 
     // Compute rate of change for quaternion
-    let qDot:Quaternion<f64> = q * Quaternion::new(0.0f64, gyroscope[0], gyroscope[1], gyroscope[2])
-        * 0.5 - Quaternion::new(step[0], step[1], step[2], step[3]) * self.beta;
+    let qDot = q * Quaternion::from_parts(zero, gyroscope)
+        * half - Quaternion::new(step[0], step[1], step[2], step[3]) * self.beta;
 
     // Integrate to yield quaternion
     self.quat = na::normalize( &(q + qDot * self.sample_period) );
@@ -79,31 +83,36 @@ impl Ahrs {
   }
 
   /// Updates the current state quaternion using 6dof IMU values.
-  pub fn update_imu( &mut self, gyroscope: Vector3<f64>, accelerometer: Vector3<f64> ) -> bool {
+  pub fn update_imu( &mut self, gyroscope: Vector3<N>, accelerometer: Vector3<N> ) -> bool {
     let q = self.quat;
 
+    let zero: N = na::zero();
+    let two: N = na::cast(2.0);
+    let four: N = na::cast(4.0);
+    let half: N = na::cast(0.5);
+
     // Normalize accelerometer measurement
-    let accel = match try_normalize(&accelerometer, 0.0f64) {
+    let accel = match try_normalize(&accelerometer, zero) {
       Some(n) => n,
       None => { return false; }
     };
 
     // Gradient descent algorithm corrective step
-    let F = Vector4::new( 2.0*(      q[1]*q[3] - q[0]*q[2]) - accel[0],
-                          2.0*(      q[0]*q[1] + q[2]*q[3]) - accel[1],
-                          2.0*(0.5 - q[1]*q[1] - q[2]*q[2]) - accel[2],
-                          0.0 );
+    let F = Vector4::new( two*(      q[1]*q[3] - q[0]*q[2]) - accel[0],
+                          two*(      q[0]*q[1] + q[2]*q[3]) - accel[1],
+                          two*(half - q[1]*q[1] - q[2]*q[2]) - accel[2],
+                          zero );
 
-    let J_t = Matrix4::new( -2.0*q[2], 2.0*q[1],            0.0, 0.0,
-                             2.0*q[3], 2.0*q[0], -4.0*q[1], 0.0,
-                            -2.0*q[0], 2.0*q[3], -4.0*q[2], 0.0,
-                             2.0*q[1], 2.0*q[2],            0.0, 0.0 );
+    let J_t = Matrix4::new( -two*q[2], two*q[1],       zero, zero,
+                             two*q[3], two*q[0], -four*q[1], zero,
+                            -two*q[0], two*q[3], -four*q[2], zero,
+                             two*q[1], two*q[2],       zero, zero );
 
-    let step: Vector4<f64> = na::normalize(&(J_t * F));
+    let step = na::normalize(&(J_t * F));
 
     // Compute rate of change of quaternion
-    let qDot:Quaternion<f64> = (q * Quaternion::new(0.0f64, gyroscope[0], gyroscope[1], gyroscope[2]))
-        * 0.5 - Quaternion::new(step[0], step[1], step[2], step[3]) * self.beta;
+    let qDot = (q * Quaternion::from_parts(zero, gyroscope))
+        * half - Quaternion::new(step[0], step[1], step[2], step[3]) * self.beta;
 
     // Integrate to yield quaternion
     self.quat = na::normalize( &(q + qDot * self.sample_period) );
